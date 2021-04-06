e5cf883b5f961a7ec88010976d0d9ab1b7868aba
Roei <roei.webd@gmail.com>
2021-04-06 20:53:05 +0300

    added literal

--- types.ts
+++ types.ts
@@ -27,34 +27,45 @@ function getFirstName(x: number[] | string) {
 
 //Alias
 type Point = {
-    x: number;
-    y: number;
-  };
-  
-  // Exactly the same as the earlier example
-  function printCoord(pt: Point) {
-    console.log("The coordinate's x value is " + pt.x);
-    console.log("The coordinate's y value is " + pt.y);
-  }
-  
-  printCoord({ x: 100, y: 100 });
+  x: number;
+  y: number;
+};
+
+// Exactly the same as the earlier example
+function printCoord(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 100, y: 100 });
 
 //Interface
 interface PointB {
-    x: number;
-    y: number;
-  }
-  
-  function printCoordB(pt: PointB) {
-    console.log("The coordinate's x value is " + pt.x);
-    console.log("The coordinate's y value is " + pt.y);
-  }
-  
-  printCoordB({ x: 100, y: 100 });
+  x: number;
+  y: number;
+}
 
-  interface T{
-    
-  }
-  //Type assertion
-  const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
-  const a = ("expr" as any) as T;
+function printCoordB(pt: PointB) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoordB({ x: 100, y: 100 });
+
+interface T {}
+//Type assertion
+const myCanvas = document.getElementById("main_canvas") as HTMLCanvasElement;
+const a = ("expr" as any) as T;
+
+//Literal
+let x: "hello" = "hello";
+// OK
+x = "hello";
+// ...
+x = "howdy";
+
+function printText(s: string, alignment: "left" | "right" | "center") {
+  // ...
+}
+printText("Hello, world", "left");
+printText("G'day, mate", "centre");
