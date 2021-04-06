dc0374468716504dd09c7072f9cb10a9ab22feda
Roei <roei.webd@gmail.com>
2021-04-06 12:17:41 +0300

    added optional

--- types/types.ts
+++ types/types.ts
@@ -1,15 +1,26 @@
-
+//Optionl
+function printName(obj: { first: string; last?: string }) {
+  // Error - might crash if 'obj.last' wasn't provided!
+  console.log(obj.last.toUpperCase());
+  //Object is possibly 'undefined'.
+  if (obj.last !== undefined) {
+    // OK
+    console.log(obj.last.toUpperCase());
+  }
+  // A safe alternative using modern JavaScript syntax:
+  console.log(obj.last?.toUpperCase());
+}
 
 //Union type
-function hello(x:string[] | string){
-    if(Array.isArray(x)){
-        console.log("hello, " + x.join(" and "));   
-    } else {
-        console.log("welcome " + name);
-    }
-};
+function hello(x: string[] | string) {
+  if (Array.isArray(x)) {
+    console.log("hello, " + x.join(" and "));
+  } else {
+    console.log("welcome " + name);
+  }
+}
 
 // Return type is inferred as number[] | string
-function getFirstName(x:number[] | string){
-    return x.slice(0,3);
+function getFirstName(x: number[] | string) {
+  return x.slice(0, 3);
 }
